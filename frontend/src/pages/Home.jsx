5224c73b4c8dc0674780e40e28982e2b4ecd352c
VladMixline <user@example.com>
2025-12-21 21:17:13 +0300

    Unify text case in navigation links on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,14 +8,14 @@ function Home({ chapters }) {
         <p className="subtitle">Учебное пособие</p>
         
         <nav className="tabs-nav">
-          <Link to="/introduction" className="tab-link">Введение</Link>
-          <Link to="/chapter1" className="tab-link">ТЕОРИЯ ФОРМАЛЬНЫХ ГРАММАТИК И ЯЗЫКОВ</Link>
-          <Link to="/chapter2" className="tab-link">ТЕОРИЯ ТРАНСЛЯЦИИ</Link>
-          <Link to="/chapter3" className="tab-link">ОРГАНИЗАЦИЯ ТАБЛИЦ СИМВОЛОВ</Link>
-          <Link to="/chapter4" className="tab-link">ОПТИМИЗАЦИЯ КОДА</Link>
-          <Link to="/chapter5" className="tab-link">ОРГАНИЗАЦИЯ ДИАЛОГА В ВЫЧИСЛИТЕЛЬНЫХ СИСТЕМАХ</Link>
-          <Link to="/exercises" className="tab-link">УПРАЖНЕНИЯ</Link>
-          <Link to="/coursework" className="tab-link">КУРСОВАЯ РАБОТА</Link>
+          <Link to="/introduction" className="tab-link"><span>Введение</span></Link>
+          <Link to="/chapter1" className="tab-link"><span>Теория формальных грамматик и языков</span></Link>
+          <Link to="/chapter2" className="tab-link"><span>Теория трансляции</span></Link>
+          <Link to="/chapter3" className="tab-link"><span>Организация таблиц символов</span></Link>
+          <Link to="/chapter4" className="tab-link"><span>Оптимизация кода</span></Link>
+          <Link to="/chapter5" className="tab-link"><span>Организация диалога в вычислительных системах</span></Link>
+          <Link to="/exercises" className="tab-link"><span>Упражнения</span></Link>
+          <Link to="/coursework" className="tab-link"><span>Курсовая работа</span></Link>
         </nav>
       </div>
     </div>
